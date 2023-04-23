b1be403d1e5e80868263d815b9a3b57a0162cdfe
diamecjs <diame.cjs@gmail.com>
2023-04-22 23:06:39 -0500

    fg

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />}  />
         <Route exact path="/certifications" element={<Certifications />} />
-      </Routes>
+      </Routes> 
     </BrowserRouter>
   </div>
   );
