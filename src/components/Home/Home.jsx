d6d737ab27c01db486463f4b968eb9ee784a4dce
diamecjs <diame.cjs@gmail.com>
2023-05-09 13:07:01 -0500

    sdxfcg

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,13 +6,13 @@ import ContactMe from "../ContactMe/ComtactMe";
 import Projects from "../Projects/Projects";
 import Footer from "../Footer/Footer";
 import Technologies from "../Technologies/Technologies";
-
+import styles from "./Home.module.css";
 
 
 const Home = () => {
 
   return (
-    <div>
+    <div className={styles["home-container"]}>
       <Navbar />
       <Presentacion />
       <div id="about-me">
